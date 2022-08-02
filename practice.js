e5e0d4b5a82a1a0357bf600358dadd2853c89340
Mehedi hassan <mh2863268@gmail.com>
2022-08-02 16:15:59 +0600

    learning github

---
+++ practice.js
@@ -0,0 +1,10 @@
+const Num = (65,32,45,74)
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (i % 2 == 0) {
+        console.log('this is a even number')
+    }
+    else{
+        CONSOLE.LOg('this is an odd number')
+    }
+}
